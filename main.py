cb7766a8195eff30f2f150e14a930463a43d9fe6
Gerson <gadonayh@gmail.com>
2023-08-24 07:55:18 -0600

    app finished and working on Appengine

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def getMethod():
+    print('Im in root')
     return render_template('index.html')
 
 @app.route("/hello")
 def hellopage():
+    print("I'm in hello")
     return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port="8080",debug=True)
+
